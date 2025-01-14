848ed812ba8a5502d2cd4c9e42ffd6e085841d77
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-01-14 07:28:12 -0400

    Se agregó la integracion con cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
+
+//import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+
   // output: 'static',
-  adapter: node({
-    mode: "standalone",
-  }),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  integrations: [mdx(), sitemap()],
+
+  adapter: cloudflare(),
 });
