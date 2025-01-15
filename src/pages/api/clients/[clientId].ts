0111292bbdf3fb7643f0ae03ce34051e82c082ec
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-01-15 17:52:20 -0400

    Se creó la configuración a una base de datos remota con turso

--- src/pages/api/clients/[clientId].ts
+++ src/pages/api/clients/[clientId].ts
@@ -1,51 +1,92 @@
 import type { APIRoute } from "astro";
+import { Clients, db, eq } from "astro:db";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
-  const { clientId } = params;
+  const clientId = +(params.clientId ?? "0");
 
-  return new Response(
-    JSON.stringify({
-      method: "GET",
-      id: clientId,
-    }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const clients = await db
+    .select()
+    .from(Clients)
+    .where(eq(Clients.id, clientId));
+
+  if (clients.length === 0) {
+    return new Response(
+      JSON.stringify({ msg: `Client with id ${clientId} not found` }),
+      {
+        status: 404,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return new Response(JSON.stringify(clients.at(0)), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 export const PUT: APIRoute = async ({ params, request }) => {
-  const { clientId } = params;
+  const clientId = +(params.clientId ?? "0");
+  try {
+    const { id, ...body } = await request.json();
 
-  return new Response(
-    JSON.stringify({
-      method: "PUT",
-      id: clientId,
-    }),
-    {
+    const results = await db
+      .update(Clients)
+      .set(body)
+      .where(eq(Clients.id, clientId));
+
+    const updatedClient = await db
+      .select()
+      .from(Clients)
+      .where(eq(Clients.id, +clientId));
+
+    return new Response(JSON.stringify(updatedClient), {
       status: 201,
       headers: {
         "Content-Type": "application/json",
       },
-    }
-  );
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error,
+      }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 };
 
 export const DELETE: APIRoute = async ({ params, request }) => {
-  const { clientId } = params;
+  const clientId = +(params.clientId ?? "0");
+
+  const { rowsAffected } = await db
+    .delete(Clients)
+    .where(eq(Clients.id, clientId));
+
+  if (rowsAffected > 0) {
+    return new Response(JSON.stringify({ msg: "Deleted" }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
   return new Response(
-    JSON.stringify({
-      method: "DELETE",
-      id: clientId,
-    }),
+    JSON.stringify({ msg: `Client with id ${clientId} not found` }),
     {
-      status: 200,
+      status: 404,
       headers: {
         "Content-Type": "application/json",
       },
--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,31 +1,48 @@
 import type { APIRoute } from "astro";
+import { Clients, db } from "astro:db";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
-  return new Response(
-    JSON.stringify({
-      method: "GET",
-    }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const users = await db.select().from(Clients);
+
+  return new Response(JSON.stringify(users), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 export const POST: APIRoute = async ({ params, request }) => {
-  return new Response(
-    JSON.stringify({
-      method: "POST",
-    }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  try {
+    const { id, ...body } = await request.json();
+
+    const { lastInsertRowid } = await db.insert(Clients).values(body);
+
+    return new Response(
+      JSON.stringify({
+        id: +(lastInsertRowid?.toString() ?? "0"),
+        ...body,
+      }),
+      {
+        status: 201,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error,
+      }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 };
